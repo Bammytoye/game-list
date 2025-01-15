9569b57a8462b22b6905c972d4c031e663b3b267
bammytoye <bamigbalatoyese@gmail.com>
2025-01-15 09:47:38 +0100

    header and theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import { ThemeContext } from "./Context/ThemeContext";
 
 function App() {
   const [theme, setTheme] = useState("dark"); 
+
+  useEffect(() => {
+    setTheme(localStorage.getItem('theme') ? localStorage.getItem('theme') : 'dark')
+  }, [])
 
   return (
     <ThemeContext.Provider value={{ theme, setTheme }}>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,10 @@
 
 function Home() {
     return (
-        <div>Home</div>
+        <div className='grid grid-cols-4 gap-2'>
+            <div className="hodden md:block bg-red-400 h-full px-3">Genre</div>
+            <div className="col-span-4 md:col-span-3 bg-blue-400 px-3">Genre List</div>
+        </div>
     )
 }
 
